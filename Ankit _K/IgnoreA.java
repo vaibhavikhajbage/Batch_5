68470b137e8c9bf4fbf5692f452b0442b7e8d63f
MyLapi <MyLapi@DESKTOP-FF5R8SR>
2022-12-14 23:47:22 +0530

    ignore a program

---
+++ Ankit _K/IgnoreA.java
@@ -0,0 +1,10 @@
+package com.newBasics;
+
+public class IgnoreA {
+	public static void main(String[] args) {
+		String s = "Java is a simplest programming langauge";
+     System.out.println(s.replace("a", " "));
+	}
+	
+
+}
