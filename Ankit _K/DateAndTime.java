cc055a7e4dbfc94c998f11d0031fe6cf0bf8579f
MyLapi <MyLapi@DESKTOP-FF5R8SR>
2022-12-14 23:43:44 +0530

    Date and Time

---
+++ Ankit _K/DateAndTime.java
@@ -0,0 +1,17 @@
+package com.DateTime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateAndTime {
+
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		
+		LocalTime time = LocalTime.now();
+		System.out.println(time);
+
+	}
+
+}
