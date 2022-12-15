14d812fd1c9da14a9cccff3f71ad49801baadad2
win10 <win10@DESKTOP-ONOV44A>
2022-12-15 20:17:01 +0530

    first stage

---
+++ Batch5/Vaishnavi_Paraskar/AdditionMultiplication.java
@@ -0,0 +1,13 @@
+package Com.Gargi;
+
+public class AdditionMultiplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
