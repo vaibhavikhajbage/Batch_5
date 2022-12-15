6885fde219a37993fe0df7f1f11e6125cf0f8b01
DELL <DELL@DESKTOP-8LRT329>
2022-12-15 13:09:16 +0530

    xyd

---
+++ bhushan_gurnule/calculation.java
@@ -0,0 +1,21 @@
+package whileprog;
+import java.util.Scanner;
+public class calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		if(i%2==1) {
+			System.out.println("odd");
+		}
+		else
+			System.out.println("even");
+		
+			
+		}
+
+
+	}
+
+
